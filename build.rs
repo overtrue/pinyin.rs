bab7ead1cff4f5b7d080b5ca22331c7ae83e5d0e
overtrue <anzhengchao@gmail.com>
2024-09-05 21:30:26 +0800

    wip

--- build.rs
+++ build.rs
@@ -2,40 +2,23 @@ use std::env;
 use std::fs::{write, File, OpenOptions};
 use std::io::prelude::*;
 use std::path::Path;
+use std::string::ToString;
 
 const DATA_PATH: &str = "data";
-const PINYIN_CHARS_FILE: &str = "chars.txt";
-const PINYIN_WORDS_FILE: &str = "words.txt";
 const PINYIN_SURNAMES_FILE: &str = "surnames.txt";
 const PINYIN_HETERONYMS_FILE: &str = "heteronyms.txt";
 
 fn main() {
-    init();
+    cleanup();
     generate_chars();
-    // generate_words();
-    // generate_surnames();
-    // generate_heteronyms();
+    generate_words();
+    generate_surnames();
+    generate_heteronyms();
 }
 
-fn init() {
-    // current directory
-    // create target files
-    for file in [
-        PINYIN_CHARS_FILE,
-        PINYIN_WORDS_FILE,
-        PINYIN_SURNAMES_FILE,
-        PINYIN_HETERONYMS_FILE,
-    ] {
-        let path = Path::new("./data").join(file);
-        let mut file = OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .create(true)
-            .open(&path)
-            .unwrap();
-
-        writeln!(file, "").expect("Failed to write to file");
-    }
+fn cleanup() {
+    std::fs::remove_dir_all(DATA_PATH).unwrap_or(());
+    std::fs::create_dir(DATA_PATH).expect("Failed to create data directory");
 }
 
 fn generate_chars() {
@@ -54,20 +37,31 @@ fn generate_chars() {
         }
     }
 
-    // 将结果写入文件
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(Path::new(DATA_PATH).join(PINYIN_CHARS_FILE))
-        .unwrap();
+    let chunk_size = data.len() / 10;
 
-    for (chinese, pinyin) in data.iter() {
-        writeln!(
-            file,
-            "{}: {}",
-            chinese,
-            pinyin
-        ).expect("Failed to write chars to file");
+    let mut count = 0;
+    for (unicode, pinyin) in data.iter() {
+        // unicode: "U+4E00"
+        let code_point = u32::from_str_radix(&unicode[2..], 16).unwrap();
+
+        let chunk_file_name = format!("chars_{}.txt", count / chunk_size);
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(Path::new(DATA_PATH).join(chunk_file_name))
+            .unwrap();
+
+        if let Some(chinese) = char::from_u32(code_point) {
+            writeln!(
+                file,
+                "{}: {}",
+                chinese,
+                pinyin
+            ).expect("Failed to write chars to file");
+        }
+
+        count += 1;
     }
 }
 
@@ -87,19 +81,26 @@ fn generate_words() {
         }
     }
 
-    // 将结果写入文件
-    let mut file = OpenOptions::new()
-        .truncate(true)
-        .open(Path::new(DATA_PATH).join(PINYIN_WORDS_FILE))
-        .unwrap();
+    let chunk_size = data.len() / 10;
+    let mut count = 0;
 
     for (chinese, pinyin) in data.iter() {
+        let chunk_file_name = format!("words_{}.txt", count / chunk_size);
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(Path::new(DATA_PATH).join(chunk_file_name))
+            .unwrap();
+
         writeln!(
             file,
             "{}: {}",
             chinese,
             pinyin
         ).expect("Failed to write words to file");
+
+        count += 1;
     }
 }
 
@@ -116,8 +117,10 @@ fn generate_surnames() {
 
     // 将结果写入文件
     let mut file = OpenOptions::new()
-        .append(true)
-        .open(Path::new(DATA_PATH).join(PINYIN_SURNAMES_FILE))
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(Path::new(DATA_PATH).join("surname.txt"))
         .unwrap();
 
     for (chinese, pinyin) in data.iter() {
@@ -140,16 +143,19 @@ fn generate_heteronyms() {
 
     // 将结果写入文件
     let mut file = OpenOptions::new()
-        .append(true)
-        .open(Path::new(DATA_PATH).join(PINYIN_HETERONYMS_FILE))
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(Path::new(DATA_PATH).join("heteronyms.txt"))
         .unwrap();
 
-    writeln!(
-        file,
-        "const PINYIN_HETERONYMS: [&str; {}] = {:#?};",
-        data.len(),
-        data
-    ).expect("Failed to write heteronyms to file");
+    data.join("\n").lines().for_each(|line| {
+        writeln!(
+            file,
+            "{}",
+            line
+        ).expect("Failed to write heteronyms to file");
+    });
 }
 
 
